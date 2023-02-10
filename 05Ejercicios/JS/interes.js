e49d3b36f82fbf03cb58f010539904c0d58762b3
ferkazares <f.cazares.h@edu.utc.mx>
2023-02-09 20:56:17 -0600

    JS
    Algoritmia tarea

--- 05Ejercicios/JS/interes.js
+++ 05Ejercicios/JS/interes.js
@@ -22,7 +22,26 @@ function interes(){
 }
 
 function borrardatos(){
-
+    document.formulario.saldo.value="";
+    document.formulario.venta.value="";
+    document.formulario.totI.value="";
     document.formulario.cantidad.value= "";
     document.formulario.sueldoI.value= "";
+
 }
+
+function comision(){
+    var saldo1 = document.formulario.saldo.value;
+    var ventas1=document.formulario.venta.value;
+    var sueldoBase = parseInt(saldo1);
+    var ventas = parseInt(ventas1);
+    //inician las operaciones para las comisiones
+    var porcentaje = ventas/10;
+    var comision=sueldoBase*porcentaje;
+    var total1 = sueldoBase+comision;
+
+    document.formulario.totI.value= "$" + total1;
+
+}
+
+
