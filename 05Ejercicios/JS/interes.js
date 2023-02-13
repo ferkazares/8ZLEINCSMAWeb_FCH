f0376ead596c25ccded25fa885d210799d7565e1
ferkazares <f.cazares.h@edu.utc.mx>
2023-02-13 08:42:24 -0600

    Ejercicios
    Ejercicios

--- 05Ejercicios/JS/interes.js
+++ 05Ejercicios/JS/interes.js
@@ -22,14 +22,21 @@ function interes(){
 }
 
 function borrardatos(){
-    document.formulario.saldo.value="";
-    document.formulario.venta.value="";
-    document.formulario.totI.value="";
-    document.formulario.cantidad.value= "";
-    document.formulario.sueldoI.value= "";
-
+    document.formulario.saldo.value ="";
+    document.formulario.venta.value ="";
+    document.formulario.totI.value ="";
+    document.formulario.cantidad.value = "";
+    document.formulario.sueldoI.value = "";
+    document.formulario.totC.value = "";
+    document.formulario.compra.value = "";
+    document.formulario.calif1.value ="";
+    document.formulario.calif2.value ="";
+    document.formulario.calif3.value ="";
+    document.formulario.Efinal.value ="";
+    document.formulario.Tfinal.value =""
+    document.formulario.Califs.value =""
 }
-
+//funcion para problema 2
 function comision(){
     var saldo1 = document.formulario.saldo.value;
     var ventas1=document.formulario.venta.value;
@@ -39,9 +46,28 @@ function comision(){
     var porcentaje = ventas/10;
     var comision=sueldoBase*porcentaje;
     var total1 = sueldoBase+comision;
-
     document.formulario.totI.value= "$" + total1;
 
 }
+//funcion para problema 3
+function descCompra(){
+    var compraInt = parseInt(document.formulario.compra.value);
+    var descuento = compraInt*0.15;
+    var totalDesc = compraInt-descuento;
+    document.formulario.totC.value ="$"+totalDesc;
+}
 
+function califFinal(){
+    var calif1Int= parseFloat(document.formulario.calif1.value);
+    var calif2Int= parseFloat(document.formulario.calif2.value);
+    var calif3Int= parseFloat(document.formulario.calif3.value);
+    var EFinalInt= parseFloat(document.formulario.Efinal.value);
+    var TFinalInt= parseFloat(document.formulario.Tfinal.value);
+    var porceCalif=((calif1Int+calif2Int+calif3Int))/3*0.55;    
+    var porceEFinal = EFinalInt*0.30;
+    var porceTFinal = TFinalInt*0.15;
+    var califTotal = porceCalif + porceEFinal + porceTFinal; 
+
+    document.formulario.Califs.value =califTotal;
+}
 
